5371a8901ec11f7627ad7ced4b79e51cd562f49b
Eddie <eddie.ch.89d@gmail.com>
2021-03-18 22:00:29 +0100

    music in sonic pi

--- sonic_1.rb
+++ sonic_1.rb
@@ -16,8 +16,7 @@ live_loop :main do
   end
 end
 
-
-live_loop :cymbals do
+live_loop :cymbals, sync: :main do
   sync :main
   15.times do
     sample :drum_cymbal_closed
@@ -25,3 +24,16 @@ live_loop :cymbals do
   end
   sample :drum_cymbal_open
 end
+
+live_loop :elec, sync: :cymbals do
+  #sample :elec_plip, beat_stretch: 2, amp: 0.5
+  sleep 3;
+end
+
+live_loop :robot, sync: :main do
+  #sample :glitch_robot1, beat_stretch: 2
+  sleep 16
+end
+
+
+
